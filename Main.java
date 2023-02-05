327dbe375e1aceab44b8d750f45b40825f56812a
mafik_tolik <mafik_tolik@mail.ru>
2023-02-05 20:50:20 +0300

    Commit2

--- Main.java
+++ Main.java
@@ -3,62 +3,60 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        // ex1();
-        // ex2();
-        // ex3();
-        // ex4();
+        ex1();
+        ex2();
+        ex3();
+        ex4();
         ex5();
     }
 
     static void ex1() {
 
-        System.out.print("\nЗадание 1. Вычислить n-ое треугольного число(сумма чисел от 1 до n).\n");
-        System.out.println("Введите n-ое треугольного число:");
+        System.out.print("\nЗадание 1. Вычислить n-ое треугольного число (сумма чисел от 1 до n).\n");
+        System.out.print("Введите n-ое треугольного число: ");
         Scanner scanner = new Scanner(System.in);
-        int n = Integer.parseInt(scanner.nextLine());
-        scanner.close();
+        int n = scanner.nextInt();
 
         int sum = 0;
 
         for (int i = 1; i <= n; i++) {
             sum += i;
         }
-        System.out.printf("Cумма чисел от 1 до %d -> %d", n, sum);
+        System.out.printf("Cумма чисел от 1 до %d -> %d\n", n, sum);
     }
 
     static void ex2() {
 
-        System.out.print("\nЗадание 2. Вычислить n! (произведение чисел от 1 до n).\n");
-        System.out.println("Введите n:");
+        System.out.println("\nЗадание 2. Вычислить n! (произведение чисел от 1 до n).");
+        System.out.print("Введите n: ");
         Scanner scanner = new Scanner(System.in);
-        int num = Integer.parseInt(scanner.nextLine());
-        scanner.close();
+        int num = scanner.nextInt();
 
         int mult = 1;
 
         for (int i = 1; i <= num; i++) {
             mult *= i;
         }
-        System.out.printf("%d! -> %d", num, mult);
+        System.out.printf("%d! -> %d\n", num, mult);
     }
 
     static void ex3() {
 
-        System.out.print("\nЗадание 3. Вывести все простые числа от 1 до 1000 (простые числа - это числа " +
+        System.out.println("\nЗадание 3. Вывести все простые числа от 1 до 1000 (простые числа - это числа " +
                 "которые делятся только на себя и на единицу без остатка. Чтобы найти остаток от деления " +
-                "используйте оператор % , например 10%3 будет равно единице).\n");
+                "используйте оператор % , например 10%3 будет равно единице).");
         int limit = 1000;
         for (int i = 1; i <= limit; i++) {
 
             if (i > 0 && i < 4) {
-                System.out.println(i);
+                System.out.print(i + ", ");
             } else {
                 boolean flag = true;
                 int j = 2;
                 while (flag) {
                     if (i % j == 0) {
                         if (i == j) {
-                            System.out.println(i);
+                            System.out.print(i + ", ");
                         }
                         flag = false;
                     } else {
@@ -70,33 +68,31 @@ public class Main {
     }
 
     static void ex4() {
-        System.out.print("\nЗадание 4. Реализовать простой калькулятор (Введите первое число... Введите " +
+        System.out.print("\n\nЗадание 4. Реализовать простой калькулятор (Введите первое число... Введите " +
                 "второе число... Укажите операцию, которую надо выполнить с этими числами... Ответ: ...)\n");
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Введите первое число:");
-        int num1 = Integer.parseInt(scanner.nextLine());
+        double num1 = Double.parseDouble(scanner.nextLine());
 
         System.out.println("Введите второе число:");
-        int num2 = Integer.parseInt(scanner.nextLine());
+        double num2 = Double.parseDouble(scanner.nextLine());
 
         System.out.println("Укажите операцию, которую надо выполнить с этими числами (+, -, * или /):");
         String oper = scanner.nextLine();
 
-        scanner.close();
-
         switch (oper) {
             case "+":
-                System.out.printf("%d + %d = %d", num1, num2, num1 + num2);
+                System.out.printf("%f + %f = %f", num1, num2, num1 + num2);
                 break;
             case "-":
-                System.out.printf("%d - %d = %d", num1, num2, num1 - num2);
+                System.out.printf("%f - %f = %f", num1, num2, num1 - num2);
                 break;
             case "*":
-                System.out.printf("%d * %d = %d", num1, num2, num1 * num2);
+                System.out.printf("%f * %f = %f", num1, num2, num1 * num2);
                 break;
             case "/":
-                System.out.printf("%d / %d = %d", num1, num2, num1 / num2);
+                System.out.printf("%f / %f = %f", num1, num2, num1 / num2);
                 break;
             default:
                 System.out.println("Указана неверная операция");
